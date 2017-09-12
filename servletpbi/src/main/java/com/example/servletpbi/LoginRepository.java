b8fc03f125ac3d987a30c4d48f155a693b2943a1
Wisesa <sesa.fentika@gmail.com>
2017-09-12 14:59:36 +0800

    Created project for servlet

---
+++ servletpbi/src/main/java/com/example/servletpbi/LoginRepository.java
@@ -0,0 +1,7 @@
+package com.example.servletpbi;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LoginRepository extends CrudRepository<User, Long>{
+
+}
---
+++ servletpbi/src/main/java/com/example/servletpbi/ServletpbiApplication.java
@@ -0,0 +1,12 @@
+package com.example.servletpbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletpbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletpbiApplication.class, args);
+	}
+}
